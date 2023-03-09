119c1b41445f2a564463b141aa53b93400ea0da4
hjwucc <wmnihaoya@gmail.com>
2023-03-10 00:46:37 +0800

    修改:样式

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,8 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-// var script = document.createElement("script");
-// script.setAttribute("type","text/javascript");
-// script.setAttribute("id","myhk");
-// script.setAttribute("src","https://myhkw.cn/api/player/166688878991");
-// script.setAttribute("key","166688878991");
-// script.setAttribute("m","1");
-// document.documentElement.appendChild(script);
+var script = document.createElement("script");
+script.setAttribute("type","text/javascript");
+script.setAttribute("id","myhk");
+script.setAttribute("src","https://myhkw.cn/api/player/166688878991");
+script.setAttribute("key","166688878991");
+script.setAttribute("m","1");
+document.documentElement.appendChild(script);
