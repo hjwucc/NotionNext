121f19c726f00e55a3566d605ec8291f1392ff6b
hjwucc <wmnihaoya@gmail.com>
2023-03-09 21:29:40 +0800

    增加:音乐插件

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,2 +1,8 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-<script src="https://myhkw.cn/api/player/166688878991" id="myhk" key="166688878991" m="1"></script>
+var script = document.createElement("script");
+script.setAttribute("type","text/javascript");
+script.setAttribute("id","myhk");
+script.setAttribute("src","https://myhkw.cn/api/player/166688878991");
+script.setAttribute("key","166688878991");
+script.setAttribute("m","1");
+document.documentElement.appendChild(script);
