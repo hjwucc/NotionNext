e59ac82efaef9d189ec9d8cb83c0c4b82960bd1d
hjwucc <wmnihaoya@gmail.com>
2023-03-09 23:06:31 +0800

    修改:底部说明

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -20,13 +20,12 @@ export const Footer = (props) => {
        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
      }`}
    >
-     <DarkModeButton className='text-center pt-4'/>
      <hr className="border-gray-200 dark:border-gray-600" />
      <div className="my-4 text-sm leading-6">
        <div className="flex align-baseline justify-between flex-wrap">
          <p>
            © {BLOG.AUTHOR} {copyrightDate}
-           <Vercel />
+           <DarkModeButton className='text-right pt-4'/>
          </p>
        </div>
      </div>
